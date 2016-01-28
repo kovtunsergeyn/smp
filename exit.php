269d9c1921b62688caa830fab58f72e607ca5761
Kovtun Sergei <s.kovtun@bars-open.ru>
2016-01-28 17:50:42 +0300

    доработал логаут - при выходе не авторизовывался

---
+++ exit.php
@@ -0,0 +1,23 @@
+<?php
+
+function theEnd() {
+
+    include "dbconnect.php";
+
+    try {
+    $SQL = 'UPDATE users SET user_hash=0 WHERE user_id=:user_id';
+    $s = $pdo->prepare($SQL);
+    $s->bindValue(':user_id', $_COOKIE['id']);
+    $s->execute();
+} catch (PDOException $connect_error) {
+    $error = 'Не удалось выполнить запрос' . $connect_error->getMessage();
+    exit();
+}
+setcookie("id", "", time() - 3600*24*30*12, "/");
+setcookie("hash", "", time() - 3600*24*30*12, "/");
+
+header('Location:register.php');
+include "registration_page.html.php";
+}
+
+theEnd();
